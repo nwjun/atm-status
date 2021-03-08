61f71fe3a257383f42cfe6bd0c21234d670071bc
nwjun <nicolenahwj@gmail.com>
2021-03-08 22:02:23 +0800

    changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
