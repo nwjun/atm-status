80ec8f67666d307a1ea458d2984708f2f85cd633
nwjun <nicolenahwj@gmail.com>
2021-03-10 13:25:54 +0800

    Fix issues

--- public/indexSrc.js
+++ public/indexSrc.js
@@ -13,32 +13,77 @@ function reson() {
   }
 }
 
-//retrieve data from database and update to index
-fetch("/getStatus")
-  .then((response) => response.json())
-  .then((response) => {
-    const data = response;
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.database();
+  var usersRef = db.ref("status");
+  usersRef
+.orderByChild("submitted")
+.limitToLast(1)
+.on(
+  "value",
+  function (snapshot) {
+    var item = Object.values(snapshot.val())[0];
+    updateList(item);
+  },
+  function (err) {
+    console.log("The read failed: " + err);
+  }
+);
+
+
+// database.on('value', (snapshot) => {
+//   const data = snapshot.val();
+//   updateStarCount(postElement, data);
+// });
+
+function updateList(item){
+  const data = item;
     let status = data["status"];
     let contributor = data["name"];
     let reason = data["reason"];
     let date = data["submitted"];
 
     //if it is working, hide reason
-    if (status) {
+    if (status === "Working") {
       document.getElementById("rowReason").style.display = "none";
       document.getElementById("status").style.color = "green";
-      document.getElementById("status").textContent = "Working";
     } else {
+      console.log(reason);
       document.getElementById("rowReason").style.display = "block";
+      document.getElementById("reason").textContent = reason;
       document.getElementById("status").style.color = "red";
-      document.getElementById("status").textContent = "Not Working";
     }
+    document.getElementById("status").textContent = status;
     document.getElementById("contributor").textContent = contributor;
     document.getElementById("dateTime").textContent = date.replace("T", " ");
-  })
-  .catch((err) => {
-    console.log("Error:", error);
-  });
+}
+// //retrieve data from database and update to index
+// fetch("/getStatus")
+//   .then((response) => response.json())
+//   .then((response) => {
+//     const data = response;
+//     let status = data["status"];
+//     let contributor = data["name"];
+//     let reason = data["reason"];
+//     let date = data["submitted"];
+
+//     //if it is working, hide reason
+//     if (status === "Working") {
+//       document.getElementById("rowReason").style.display = "none";
+//       document.getElementById("status").style.color = "green";
+//     } else {
+//       console.log(reason);
+//       document.getElementById("rowReason").style.display = "block";
+//       document.getElementById("reason").textContent = reason;
+//       document.getElementById("status").style.color = "red";
+//     }
+//     document.getElementById("status").textContent = status;
+//     document.getElementById("contributor").textContent = contributor;
+//     document.getElementById("dateTime").textContent = date.replace("T", " ");
+//   })
+//   .catch((err) => {
+//     console.log("Error:", error);
+//   });
 
 function feedback() {
   //Show/Hide feedback form
