806601bc6314859e8766dbfa2c4e5cc6c80bac33
nwjun <nicolenahwj@gmail.com>
2021-03-10 15:50:26 +0800

    Hide credential

---
+++ public/init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOUrr3jGvZ_B2rkvu8FH3y8r_y9D8gNs8",
+    authDomain: "statuslist-a643b.firebaseapp.com",
+    databaseURL: "https://statuslist-a643b-default-rtdb.firebaseio.com",
+    projectId: "statuslist-a643b",
+    storageBucket: "statuslist-a643b.appspot.com",
+    messagingSenderId: "374688213277",
+    appId: "1:374688213277:web:4ab653dd3ec97a5ee459f4",
+    measurementId: "G-T5Y7H7HJLN"
+  };
+
+firebase.initializeApp(firebaseConfig);
